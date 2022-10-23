d650793013c0b6b5c821722db58681bef93e96e6
Jian Furquim <furquimjian@gmail.com>
2022-10-23 15:33:15 -0300

    Update: Register viewset Users

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from apps.users.views import UserViewSet
+
 router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
