1b320e181950adeb57061100d625320a1fd9884e
Jian Furquim <furquimjian@gmail.com>
2022-10-23 15:22:40 -0300

    Update: Model Users started fields

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+        db_table = 'auth_user'
+        ordering = ['pk']
+
+    def __str__(self):
+        return self.get_full_name()
