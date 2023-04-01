8e1ce53579427affbdb18819b5f5d9ccc8a85df4
Jian Furquim <furquimjian@gmail.com>
2023-04-01 15:30:57 -0300

    Update: Remove password to User serializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -6,4 +6,4 @@ from apps.users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
