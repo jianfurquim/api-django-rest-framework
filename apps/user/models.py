00ff34bb103273e5dd68e35c0b6b85e0f12790b8
Jian Furquim <furquimjian@gmail.com>
2024-03-24 20:48:41 -0300

    Add slug field in User model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.db import models
 
 
 class User(AbstractUser):
+    slug = models.SlugField(max_length=100, blank=True, unique=True, null=True)
+    is_active = models.BooleanField(_("Active"), default=False)
+
     class Meta(AbstractUser.Meta):
-        swappable = 'AUTH_USER_MODEL'
-        db_table = 'auth_user'
-        ordering = ['pk']
+        swappable = "AUTH_USER_MODEL"
+        db_table = "auth_user"
+        ordering = ["pk"]
 
     def __str__(self):
         return self.get_full_name()
