bf3fb50d96d0012ec569c6968b465e16e3f3e93f
Jian Furquim <furquimjian@gmail.com>
2024-03-24 20:33:01 -0300

    Removing views for the User app

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from apps.users.views import UserViewSet
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
